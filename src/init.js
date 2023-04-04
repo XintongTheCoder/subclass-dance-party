e18d58235c2caf3c09085ed95abf53bef799b14b
XintongTheCoder <xintongmi@gmail.com>
2023-04-04 15:36:26 -0700

    complete all bare minimum features

--- src/init.js
+++ src/init.js
@@ -65,4 +65,45 @@ $(document).ready(function () {
       }
     }
   });
+  $('.pairUpButton').on('click', function (event) {
+    // iterate through the dancers array
+    // find each pair
+    // update their positions so that they go next to each other
+    // debugger;
+    var pairs = [];
+    var visited = [];
+    for (var i = 0; i < window.dancers.length; i++) {
+      if (visited[i]) {
+        continue;
+      }
+      var currentDancer = window.dancers[i];
+      visited[i] = true;
+      let distance, closestDancer, closestDancerIndex;
+      for (var j = i + 1; j < window.dancers.length; j++) {
+        if (visited[j]) {
+          continue;
+        }
+        var pair = window.dancers[j];
+        // c = sqrt(a^2 + b^2)
+        var a = Math.abs(currentDancer.top - pair.top);
+        var b = Math.abs(currentDancer.left - pair.left);
+        var c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+        if (!closestDancer || c < distance) {
+          closestDancer = pair;
+          distance = c;
+          closestDancerIndex = j;
+        }
+      }
+      pairs.push([currentDancer, closestDancer]);
+      visited[closestDancerIndex] = true;
+    }
+    for (var i = 0; i < pairs.length; i++) {
+      var currentPair = pairs[i];
+      var dancer1 = currentPair[0];
+      var dancer2 = currentPair[1];
+      dancer2.setPosition(dancer1.top, dancer1.left - 50);
+    }
+    console.log(pairs);
+    console.log(visited);
+  });
 });
